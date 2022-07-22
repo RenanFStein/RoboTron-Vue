88f31d9b7cf3d0a3e5acc43a3f462cef7d7d2f5b
RenanFStein <renan_stein@hotmail.com>
2022-07-22 08:18:36 -0300

    Commit - Conclusão do Projeto utilizando Vue.JS

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+
+
+<template>
+  <main>
+    <RoboTron/>
+    
+    
+  </main>
+  
+</template>
+
+<script>
+import RoboTron from "./components/RoboTron.vue";
+export default {
+    name:'App',
+    components: { 
+      RoboTron 
+      }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/RoboTron.vue
@@ -0,0 +1,234 @@
+<template>
+    <div>
+        <main>
+        <section class="robotron">   
+                <img class="robo" :src="'../../public/robotron-' + (corRobotron) + '.png'" alt="Robotron">
+            <figcaption class="titulo">ROBOTRON <br>2000</figcaption>
+        </section>
+
+
+        <section class="box estatisticas">
+            <div class="estatistica">
+                <p class="estatistica-titulo">Força</p>
+                <div class="estatistica-valor">
+                    <p class="estatistica-numero">{{forca}}</p>
+                </div>
+            </div>
+            <div class="estatistica">
+                <p class="estatistica-titulo">Poder</p>
+                <div class="estatistica-valor">
+                    <p class="estatistica-numero">{{poder}}</p>
+                </div>
+            </div>
+            <div class="estatistica">
+                <p class="estatistica-titulo">Energia</p>
+                <div class="estatistica-valor">
+                    <p class="estatistica-numero">{{energia}}</p>
+                </div>
+            </div>
+            <div class="estatistica">
+                <p class="estatistica-titulo">Velocidade</p>
+                <div class="estatistica-valor">
+                    <p class="estatistica-numero">{{velocidade}}</p>
+                </div>
+            </div>
+        </section>
+
+        <section class="equipamentos">
+            <form action="" class="montador">
+                <div class="box montador-conteudo">
+                    <div class="peca">
+                        <label for="" class="peca-titulo">Braços</label>
+                        <div class="controle">
+                            <buttom class="controle-ajuste" @click="subtrair('braco')">-</buttom>
+                            <input type="text" class="controle-contador" :value="braco" id="braco">
+                            <buttom class="controle-ajuste" @click="somar('braco')">+</buttom>
+                        </div>
+                    </div>
+                    <hr class="linha">
+                    <div class="peca">
+                        <label for="" class="peca-titulo">Blindagem</label>
+                        <div class="controle">
+                            <buttom class="controle-ajuste" @click="subtrair('blindagem')">-</buttom>
+                            <input type="text" class="controle-contador" :value="blindagem" id="blindagem">
+                            <buttom class="controle-ajuste" @click="somar('blindagem')">+</buttom>
+                        </div>
+                    </div>
+                    <hr class="linha">
+                    <div class="peca">
+                        <label for="" class="peca-titulo">Núcleos</label>
+                        <div class="controle">
+                            <buttom class="controle-ajuste" @click="subtrair('nucleos')">-</buttom>
+                            <input type="text" class="controle-contador" :value="nucleos" id="nucleos">
+                            <buttom class="controle-ajuste" @click="somar('nucleos')">+</buttom>
+                        </div>
+                    </div>
+                    <hr class="linha">
+                    <div class="peca">
+                        <label for="" class="peca-titulo">Pernas</label>
+                        <div class="controle">
+                            <buttom class="controle-ajuste" @click="subtrair('pernas')">-</buttom>
+                            <input type="text" class="controle-contador" :value="pernas" id="pernas">
+                            <buttom class="controle-ajuste" @click="somar('pernas')">+</buttom>
+                        </div>
+                    </div>
+                    <hr class="linha">
+                    <div class="peca">
+                        <label for="" class="peca-titulo">Foguetes</label>
+                        <div class="controle">
+                            <buttom class="controle-ajuste" @click="subtrair('foguetes')">-</buttom>
+                            <input type="text" class="controle-contador" :value="foguetes" id="foguetes">
+                            <buttom class="controle-ajuste" @click="somar('foguetes')">+</buttom>
+                        </div>
+                    </div>
+                    <hr class="linha">
+                    <label for="color" class="peca-titulo">Escolha a color:</label>
+                     <div>
+                        <select @click="color($event)" class='form-select' name="color" id="color">
+                            <option value="Amarelo">Amarelo</option>
+                            <option value="Azul">Azul</option>
+                            <option value="Branco">Branco</option>
+                            <option value="Preto">Preto</option>
+                            <option value="Rosa">Rosa</option>
+                            <option value="Vermelho">Vermelho</option>
+                        </select>
+                    </div>                    
+                </div>
+                <input type="submit" value="Iniciar produção" class="producao" id="producao">
+            </form>
+        </section>
+    </main>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'RoboTron',
+    data() {
+        return{
+            corRobotron: "amarelo",
+
+            braco : 0,
+            blindagem : 0,
+            nucleos : 0,
+            pernas : 0,
+            foguetes : 0,
+
+            forca : 773,
+            poder : 630,
+            energia : 289,
+            velocidade: 597,
+
+            adicional : {
+                "braco": {
+                    "forca": 29,
+                    "poder": 35,
+                    "energia": -21,
+                    "velocidade": -5
+                },
+
+                "blindagem": {
+                    "forca": 41,
+                    "poder": 20,
+                    "energia": 0,
+                    "velocidade": -20
+                },
+                "nucleos":{
+                    "forca": 0,
+                    "poder": 7,
+                    "energia": 48,
+                    "velocidade": -24
+                },
+                "pernas":{
+                    "forca": 27,
+                    "poder": 21,
+                    "energia": -32,
+                    "velocidade": 42
+                },
+                "foguetes":{
+                    "forca": 0,
+                    "poder": 28,
+                    "energia": 0,
+                    "velocidade": -2
+                }
+                
+            }
+            } 
+                               
+        },
+        
+    methods: {
+        color(e) {
+            this.corRobotron = document.getElementById('color').value;
+       },
+        subtrair(partName) {
+            //const partName = e.target.parentElement.childNodes[1].id            
+            if (this[partName] > 0){
+                this[partName] -= 1
+                this.forca = this.forca - this.adicional[partName]['forca']
+                this.poder = this.poder - this.adicional[partName]['poder']
+                this.energia = this.energia - this.adicional[partName]['energia']
+                this.velocidade = this.velocidade -  this.adicional[partName]['velocidade']
+            }
+
+                                    
+       },
+        somar(partName){        
+            //const partName = e.target.parentElement.childNodes[1].id
+            this[partName] += 1     
+            this.forca = this.forca + this.adicional[partName]['forca']
+            this.poder = this.poder + this.adicional[partName]['poder']
+            this.energia = this.energia + this.adicional[partName]['energia']
+            this.velocidade = this.velocidade + this.adicional[partName]['velocidade']        
+       },
+     
+    }
+}
+    
+
+</script>
+
+<style scoped>
+.form-select {
+  display: block;
+  width: 100%;
+  padding: 0.375rem 2.25rem 0.375rem 0.75rem;
+  -moz-padding-start: calc(0.75rem - 3px);
+  font-size: 1rem;
+  font-weight: 400;
+  line-height: 1.5;
+  color: #212529;
+  background-color: #fff;
+  background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16'%3e%3cpath fill='none' stroke='%23343a40' stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='m2 5 6 6 6-6'/%3e%3c/svg%3e");
+  background-repeat: no-repeat;
+  background-position: right 0.75rem center;
+  background-size: 16px 12px;
+  border: 1px solid #ced4da;
+  border-radius: 0.375rem;
+  transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+}
+@media (prefers-reduced-motion: reduce) {
+  .form-select {
+    transition: none;
+  }
+}
+.form-select:focus {
+  border-color: #86b7fe;
+  outline: 0;
+  box-shadow: 0 0 0 0.25rem rgba(13, 110, 253, 0.25);
+}
+.form-select[multiple], .form-select[size]:not([size="1"]) {
+  padding-right: 0.75rem;
+  background-image: none;
+}
+.form-select:disabled {
+  background-color: #e9ecef;
+}
+.form-select:-moz-focusring {
+  color: transparent;
+  text-shadow: 0 0 0 #212529;
+}
+</style>
